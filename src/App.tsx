28f4ab0d68b4c73937d97806ea671dbfd5d54efe
osdoonhyun <skw1996@naver.com>
2024-03-27 22:19:57 +0900

    feat: tanstack query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { routeTree } from './routeTree.gen';
 
 const router = createRouter({ routeTree });
@@ -9,6 +10,12 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
