6e4b4cb5930dfbd48799ba11848810a39f860380
osdoonhyun <skw1996@naver.com>
2024-04-01 22:45:45 +0900

    feat: cartStore 상품 수량 수정, 상품 선택, 제거 기능 추가

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -1,11 +1,17 @@
 import create from 'zustand';
 import createSelectors from '@/store/selectors';
-import { CartProductWithQuantity } from '@/types/product';
+import { CartProduct, CartProductWithSelection } from '@/types/cart';
 
 interface cartState {
-  cart: CartProductWithQuantity[];
+  cart: CartProductWithSelection[];
 
-  addToCart: (product: CartProductWithQuantity) => void; // 상품 추가
+  addToCart: (product: CartProduct) => void; // 상품 추가
+  removeProduct: (productId: CartProduct['id']) => void;
+  toggleProduct: (productId: CartProduct['id']) => void;
+  increaseProductQuantity: (productId: CartProduct['id']) => void;
+  decreaseProductQuantity: (productId: CartProduct['id']) => void;
+  selectAllProducts: () => void;
+  unselectAllProducts: () => void;
 }
 
 const useCartStoreBase = create<cartState>()((set) => ({
@@ -26,6 +32,43 @@ const useCartStoreBase = create<cartState>()((set) => ({
         return { cart: [...state.cart, newProduct] };
       }
     }),
+  // TODO: productId === item.product.id이 맞는지 확인해볼 것.(item.id)
+  removeProduct: (productId) =>
+    set((state) => ({
+      cart: state.cart.filter((item) => item.product.id !== productId),
+    })),
+  toggleProduct: (productId) =>
+    set((state) => ({
+      cart: state.cart.map((item) =>
+        item.product.id === productId
+          ? { ...item, selected: !item.selected }
+          : item
+      ),
+    })),
+  increaseProductQuantity: (productId) =>
+    set((state) => ({
+      cart: state.cart.map((item) =>
+        item.product.id === productId
+          ? { ...item, quantity: (item.quantity ?? 0) + 1 }
+          : item
+      ),
+    })),
+  decreaseProductQuantity: (productId) =>
+    set((state) => ({
+      cart: state.cart.map((item) =>
+        item.product.id === productId
+          ? { ...item, quantity: (item.quantity ?? 1) - 1 }
+          : item
+      ),
+    })),
+  selectAllProducts: () =>
+    set((state) => ({
+      cart: state.cart.map((item) => ({ ...item, selected: true })),
+    })),
+  unselectAllProducts: () =>
+    set((state) => ({
+      cart: state.cart.map((item) => ({ ...item, selected: false })),
+    })),
 }));
 
 export default createSelectors(useCartStoreBase);
