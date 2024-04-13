768daf626eda0803d14cc35f86a2de09ce66cf9b
osdoonhyun <skw1996@naver.com>
2024-04-13 20:31:58 +0900

    feat: cartStore persist 기능 추가

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import createSelectors from '@/store/selectors';
 import { CartProduct } from '@/types/cart';
 import { Product } from '@/types/product';
+import { persist } from 'zustand/middleware';
 
 interface CartState {
   cart: CartProduct[];
@@ -14,48 +15,55 @@ interface CartState {
   decreaseProductQuantity: (productId: CartProduct['id']) => void;
 }
 
-const useCartStoreBase = create<CartState>()((set) => ({
-  cart: [],
+const useCartStoreBase = create<CartState>()(
+  persist(
+    (set) => ({
+      cart: [],
 
-  addToCart: (product) =>
-    set((state) => {
-      const isProductExist = state.cart.some(
-        (item) => item.product.id === product.product.id
-      );
+      addToCart: (product) =>
+        set((state) => {
+          const isProductExist = state.cart.some(
+            (item) => item.product.id === product.product.id
+          );
 
-      if (isProductExist) {
-        return { cart: [...state.cart] };
-      } else {
-        const newProduct = { ...product, quantity: 1 };
+          if (isProductExist) {
+            return { cart: [...state.cart] };
+          } else {
+            const newProduct = { ...product, quantity: 1 };
 
-        return { cart: [...state.cart, newProduct] };
-      }
+            return { cart: [...state.cart, newProduct] };
+          }
+        }),
+      clearCart: () => set({ cart: [] }),
+      removeProducts: (productIds) =>
+        set((state) => ({
+          cart: state.cart.filter((item) => !productIds.has(item.product.id)),
+        })),
+      removeProduct: (productId) =>
+        set((state) => ({
+          cart: state.cart.filter((item) => item.product.id !== productId),
+        })),
+      increaseProductQuantity: (productId) =>
+        set((state) => ({
+          cart: state.cart.map((item) =>
+            item.product.id === productId
+              ? { ...item, quantity: (item.quantity ?? 0) + 1 }
+              : item
+          ),
+        })),
+      decreaseProductQuantity: (productId) =>
+        set((state) => ({
+          cart: state.cart.map((item) =>
+            item.product.id === productId
+              ? { ...item, quantity: (item.quantity ?? 1) - 1 }
+              : item
+          ),
+        })),
     }),
-  clearCart: () => set({ cart: [] }),
-  removeProducts: (productIds) =>
-    set((state) => ({
-      cart: state.cart.filter((item) => !productIds.has(item.product.id)),
-    })),
-  removeProduct: (productId) =>
-    set((state) => ({
-      cart: state.cart.filter((item) => item.product.id !== productId),
-    })),
-  increaseProductQuantity: (productId) =>
-    set((state) => ({
-      cart: state.cart.map((item) =>
-        item.product.id === productId
-          ? { ...item, quantity: (item.quantity ?? 0) + 1 }
-          : item
-      ),
-    })),
-  decreaseProductQuantity: (productId) =>
-    set((state) => ({
-      cart: state.cart.map((item) =>
-        item.product.id === productId
-          ? { ...item, quantity: (item.quantity ?? 1) - 1 }
-          : item
-      ),
-    })),
-}));
+    { name: 'cart-storage' }
+  )
+);
 
-export default createSelectors(useCartStoreBase);
+const useCartStore = createSelectors(useCartStoreBase);
+
+export default useCartStore;
