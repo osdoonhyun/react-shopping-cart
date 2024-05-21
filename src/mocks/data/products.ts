8376455ce99be3ac87cd64d7edfcd06208e0db39
osdoonhyun <skw1996@naver.com>
2024-05-21 22:50:35 +0900

    feat: 상품 목록 추가

--- src/mocks/data/products.ts
+++ src/mocks/data/products.ts
@@ -2,274 +2,419 @@ import { Product } from '@/types/product';
 
 export const MOCK_PRODUCTS_DATA: Product[] = [
   {
-    id: 1,
-    name: '냉면용기(대)',
-    price: 83700,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/2/1556008840639m0.jpg',
-  },
-  {
-    id: 2,
-    name: '생새우살 (71/90) 500g 4개',
-    price: 29000,
+    id: 1000466376,
+    name: '[그레인온] 골드카무트효소 30포',
+    price: 35000,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/6b95c66a-c13d-4ccd-9df5-b1af1428a225.jpg',
+      'https://product-image.kurly.com/product/image/be332720-3259-4fa3-9423-64a4d395df07.jpg',
   },
   {
-    id: 3,
-    name: '펩시 콜라 355ml 24캔',
-    price: 83700,
+    id: 1000443345,
+    name: '[칸로] 캔디 5종 (택1)',
+    price: 3960,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/84fc0238-0239-4d0e-870b-a9daa6f2c42c.jpg',
+      'https://product-image.kurly.com/product/image/7aae11b3-348c-4935-bc2d-eab8b8f67d5f.jpg',
   },
   {
-    id: 4,
-    name: '리치스 스위트콘 대 2.95kg',
-    price: 4780,
+    id: 1000489325,
+    name: '[오트사이드] 미니 귀리 우유 3종 (200mL X 6팩)',
+    price: 7980,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/ff297e50-7d6b-4a87-8509-b387442ff877.jpg',
+      'https://product-image.kurly.com/product/image/733035e6-4952-494c-aeed-985b97a820a3.jpg',
   },
   {
-    id: 5,
-    name: '하늘푸드 스위트고로케 1kg',
-    price: 5200,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/2/1556008840639m0.jpg',
-  },
-  {
-    id: 6,
-    name: '야채고로케 (60g*18입) 1080g',
-    price: 11170,
+    id: 1000471359,
+    name: '[남양]백미당 모나카 3종(택1)',
+    price: 13500,
     imageUrl:
-      'https://cdn-mart.baemin.com/goods/custom/20200427/9751-main-01.png',
+      'https://product-image.kurly.com/product/image/4e18dd83-fafa-4c7e-b032-86ac5b235488.jpg',
   },
   {
-    id: 7,
-    name: '식자재왕 김말이튀김 야채맛 1kg',
-    price: 6580,
+    id: 5093936,
+    name: "[Kurly's] 동물복지 유정란 30구 (15구*2ea)",
+    price: 14550,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/bulk/20211210-155432/9416-main-01.jpg',
+      'https://product-image.kurly.com/product/image/706ae83b-8c51-49ef-bdf6-dea4025b2f4f.jpg',
   },
   {
-    id: 8,
-    name: '삼양 야채튀김 (60g*50±1/박스) 3kg',
-    price: 24610,
+    id: 1000489842,
+    name: '[라이블링] 애플사이다비니거 옐로우구미 60개입',
+    price: 39900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/8916edff-9fa9-4538-95c3-13d463f58a86.jpg',
+      'https://product-image.kurly.com/product/image/19887995-bbad-438b-8219-da034d859acd.jpg',
   },
   {
-    id: 9,
-    name: '미미사 분모자 (17mm) 250G',
-    price: 1440,
+    id: 1000426164,
+    name: '[제주우유] 무항생제우유 115ml X 24입',
+    price: 18900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/6ae4c431-6988-41ab-8894-7df7ee7b7cb3.jpg',
+      'https://product-image.kurly.com/product/image/afdbecb2-a88f-4252-a4ea-a24850a4998e.jpg',
   },
   {
-    id: 10,
-    name: '손수 맛있는 쌀떡볶이떡 1kg',
-    price: 2200,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/21/10221-main-01.jpg',
+    id: 5036748,
+    name: '[제스프리] 뉴질랜드 골드 키위 760g (4입X2팩) (후숙필요)',
+    price: 13900,
+    imageUrl: 'https://img-cf.kurly.com/shop/data/goods/1653038579476l0.jpeg',
   },
   {
-    id: 11,
-    name: '젓가락(종이)-웬만해선 이 맛을 막을 수 없다',
-    price: 21800,
+    id: 1000466030,
+    name: '[오뚜기]진진짜라 (135GX4)',
+    price: 5680,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/1b6e926b-52a3-4a92-8db5-fddaccdb2583.jpg',
+      'https://product-image.kurly.com/product/image/91e01e4c-68f9-4d8b-b846-aad793a139f2.jpg',
   },
   {
-    id: 12,
-    name: '[리뉴얼]젓가락(종이)-정성을 담아',
-    price: 21800,
+    id: 5132780,
+    name: '[제스프리] 뉴질랜드 골드키위 1.1kg (9~10입) (후숙필요)',
+    price: 19900,
+    imageUrl: 'https://img-cf.kurly.com/shop/data/goods/1650348343574l0.jpg',
+  },
+  {
+    id: 1000484433,
+    name: '[한끼통살] 리얼 마녀스프 4종 (택1)',
+    price: 4900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/5297837f-5ecd-4945-be2f-4a75854cd06e.jpg',
+      'https://product-image.kurly.com/product/image/49c2578d-b084-483f-8a38-6c8321d3ec2a.jpg',
   },
   {
-    id: 13,
-    name: '냉면용기(대)',
-    price: 83700,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/2/1556008840639m0.jpg',
-  },
-  {
-    id: 14,
-    name: '생새우살 (71/90) 500g 4개',
-    price: 29000,
+    id: 1000472934,
+    name: '[핀카듀에르나스 X Kurly] 피쿠알 엑스트라 버진 올리브오일 500mL',
+    price: 39800,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/6b95c66a-c13d-4ccd-9df5-b1af1428a225.jpg',
+      'https://product-image.kurly.com/product/image/99825229-925a-43eb-a3e2-d87177670c77.jpg',
   },
   {
-    id: 15,
-    name: '펩시 콜라 355ml 24캔',
-    price: 83700,
+    id: 1000496970,
+    name: '[보웰] 자연을 담은 바람 솔솔 뱀부 시어서커 여름 쇼파패드 6종 (택1)',
+    price: 42000,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/84fc0238-0239-4d0e-870b-a9daa6f2c42c.jpg',
+      'https://3p-image.kurly.com/files/86ce424c-dda8-4ab7-aadc-f8e2e4a1e839/c9886139-df28-4437-b77f-1320b97156e7.jpg',
   },
   {
-    id: 16,
-    name: '리치스 스위트콘 대 2.95kg',
-    price: 4780,
+    id: 1000454606,
+    name: '[얼라이브] 멀티 비타 구미 60구미',
+    price: 35000,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/ff297e50-7d6b-4a87-8509-b387442ff877.jpg',
+      'https://product-image.kurly.com/product/image/5e092609-b196-4a63-8ff9-29c8e6107409.jpg',
   },
   {
-    id: 17,
-    name: '하늘푸드 스위트고로케 1kg',
-    price: 5200,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/2/1556008840639m0.jpg',
-  },
-  {
-    id: 18,
-    name: '야채고로케 (60g*18입) 1080g',
-    price: 11170,
+    id: 1000462149,
+    name: '[사베] 하와이안 브레드 (4개입)',
+    price: 8800,
     imageUrl:
-      'https://cdn-mart.baemin.com/goods/custom/20200427/9751-main-01.png',
+      'https://product-image.kurly.com/product/image/967e6366-f7be-4161-8896-3a1f93e9544d.jpg',
   },
   {
-    id: 19,
-    name: '식자재왕 김말이튀김 야채맛 1kg',
-    price: 6580,
+    id: 1000463165,
+    name: '[지리산처럼] 행복한 정은씨의 흑임자 가루 150g',
+    price: 16500,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/bulk/20211210-155432/9416-main-01.jpg',
+      'https://product-image.kurly.com/product/image/a2cb0d75-54a9-476b-b7b3-2fe9cfb388ac.jpg',
   },
   {
-    id: 20,
-    name: '삼양 야채튀김 (60g*50±1/박스) 3kg',
-    price: 24610,
+    id: 5136384,
+    name: '[KF365] 당도선별 수박 4kg 이상',
+    price: 34900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/8916edff-9fa9-4538-95c3-13d463f58a86.jpg',
+      'https://product-image.kurly.com/product/image/69786586-d752-4a44-8e6c-79d655faa505.jpg',
   },
   {
-    id: 21,
-    name: '미미사 분모자 (17mm) 250G',
-    price: 1440,
+    id: 1000485275,
+    name: '[테이스터스 초이스] 오리지널 아메리카노 1병(50g)',
+    price: 4900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/6ae4c431-6988-41ab-8894-7df7ee7b7cb3.jpg',
+      'https://product-image.kurly.com/product/image/0a403dbb-3143-4b06-89dd-71a86ef216e4.jpg',
   },
   {
-    id: 22,
-    name: '손수 맛있는 쌀떡볶이떡 1kg',
-    price: 2200,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/21/10221-main-01.jpg',
-  },
-  {
-    id: 23,
-    name: '젓가락(종이)-웬만해선 이 맛을 막을 수 없다',
-    price: 21800,
+    id: 1000428305,
+    name: '[선물세트] 박준우의 오쁘띠베르 피낭시에 가르니 세트 (6개입)',
+    price: 31800,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/1b6e926b-52a3-4a92-8db5-fddaccdb2583.jpg',
+      'https://product-image.kurly.com/product/image/951fb001-ebdc-440f-84c3-fc255605ed45.jpg',
   },
   {
-    id: 24,
-    name: '[리뉴얼]젓가락(종이)-정성을 담아',
-    price: 21800,
+    id: 1000462795,
+    name: '[오일팡] 물로만 깨끗하게 만능행주 3개입&5개입 6종 (택1) ',
+    price: 18000,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/5297837f-5ecd-4945-be2f-4a75854cd06e.jpg',
+      'https://product-image.kurly.com/product/image/84b5c6cd-30aa-44cc-86cc-ca413fc80725.jpg',
   },
   {
-    id: 25,
-    name: '펩시 콜라 355ml 24캔',
-    price: 83700,
+    id: 1000481379,
+    name: '[휘태커스] 미니슬랩 아몬드 밀크초콜릿 180g',
+    price: 10900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/84fc0238-0239-4d0e-870b-a9daa6f2c42c.jpg',
+      'https://product-image.kurly.com/product/image/c8b9974e-8eeb-4f75-95af-2707bcd91ac7.jpg',
   },
   {
-    id: 26,
-    name: '리치스 스위트콘 대 2.95kg',
-    price: 4780,
+    id: 1000439151,
+    name: '[인생꼬치] 파닭 꼬치 490g(소스증정)',
+    price: 12900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/ff297e50-7d6b-4a87-8509-b387442ff877.jpg',
+      'https://product-image.kurly.com/product/image/4dbf9038-bf89-4915-a1a3-27b41b83451c.jpg',
   },
   {
-    id: 27,
-    name: '하늘푸드 스위트고로케 1kg',
-    price: 5200,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/2/1556008840639m0.jpg',
+    id: 5131916,
+    name: '[하코야] 히모카와 납작우동 (2인분)',
+    price: 8500,
+    imageUrl: 'https://img-cf.kurly.com/shop/data/goods/1654839659900l0.jpg',
   },
   {
-    id: 28,
-    name: '야채고로케 (60g*18입) 1080g',
-    price: 11170,
+    id: 1000466383,
+    name: '[그레인온] 골드카무트효소G 30포',
+    price: 25000,
     imageUrl:
-      'https://cdn-mart.baemin.com/goods/custom/20200427/9751-main-01.png',
+      'https://product-image.kurly.com/product/image/51b4055c-0e1b-46d0-8ca2-5ba07a35c7c1.jpg',
   },
   {
-    id: 29,
-    name: '식자재왕 김말이튀김 야채맛 1kg',
-    price: 6580,
+    id: 1000466226,
+    name: '[아그로몬테] 시칠리아 체리 토마토 파스타소스 3종',
+    price: 5900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/bulk/20211210-155432/9416-main-01.jpg',
+      'https://product-image.kurly.com/product/image/1243f39d-6271-4019-9a15-6a2449026465.jpg',
   },
   {
-    id: 30,
-    name: '삼양 야채튀김 (60g*50±1/박스) 3kg',
-    price: 24610,
+    id: 1000489840,
+    name: '[라이블링] 옐로우 클렌즈 450mL 30개입 3종 (택1)',
+    price: 39900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/8916edff-9fa9-4538-95c3-13d463f58a86.jpg',
+      'https://product-image.kurly.com/product/image/80aabf2e-18d5-437b-ba38-3f8e301dca35.jpg',
   },
   {
-    id: 31,
-    name: '미미사 분모자 (17mm) 250G',
-    price: 1440,
+    id: 1000443709,
+    name: '[Bread31] 31가지 곡물&씨앗 식빵',
+    price: 4900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/6ae4c431-6988-41ab-8894-7df7ee7b7cb3.jpg',
+      'https://product-image.kurly.com/product/image/2555b629-3baa-4277-89e3-d41c3e2c5717.jpg',
   },
   {
-    id: 32,
-    name: '손수 맛있는 쌀떡볶이떡 1kg',
-    price: 2200,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/21/10221-main-01.jpg',
-  },
-  {
-    id: 33,
-    name: '젓가락(종이)-웬만해선 이 맛을 막을 수 없다',
-    price: 21800,
+    id: 1000439149,
+    name: '[인생꼬치] 불닭맛 닭꼬치 400g',
+    price: 13900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/1b6e926b-52a3-4a92-8db5-fddaccdb2583.jpg',
+      'https://product-image.kurly.com/product/image/9925b9cb-0c54-482e-b956-8be0fadf7aa1.jpg',
   },
   {
-    id: 34,
-    name: '[리뉴얼]젓가락(종이)-정성을 담아',
-    price: 21800,
+    id: 1000467375,
+    name: '[햇반] 병아리콩퀴노아곤약밥 150g*3ea',
+    price: 8940,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/5297837f-5ecd-4945-be2f-4a75854cd06e.jpg',
+      'https://product-image.kurly.com/product/image/dfeba249-30a6-4c92-941b-8ecc8200acd1.jpg',
   },
   {
-    id: 35,
-    name: '펩시 콜라 355ml 24캔',
-    price: 83700,
+    id: 1000466607,
+    name: '[백설] 육수에는 1분링 멸치디포리 4gX50개입',
+    price: 10980,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/84fc0238-0239-4d0e-870b-a9daa6f2c42c.jpg',
+      'https://product-image.kurly.com/product/image/6453d904-6051-4cbd-886c-5f650d73c3f8.jpg',
   },
   {
-    id: 36,
-    name: '리치스 스위트콘 대 2.95kg',
-    price: 4780,
+    id: 1000489777,
+    name: '[플라이밀] 단백질 쉐이크 파우치 6종 (45g x 7개입) (택1)',
+    price: 35000,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/ff297e50-7d6b-4a87-8509-b387442ff877.jpg',
+      'https://product-image.kurly.com/product/image/ae52ce1a-44b4-4531-9043-3034832dc284.jpg',
   },
   {
-    id: 37,
-    name: '하늘푸드 스위트고로케 1kg',
-    price: 5200,
-    imageUrl: 'https://cdn-mart.baemin.com/goods/2/1556008840639m0.jpg',
-  },
-  {
-    id: 38,
-    name: '야채고로케 (60g*18입) 1080g',
-    price: 11170,
+    id: 1000474143,
+    name: '[풀무원] 칡냉면 (2인)',
+    price: 7980,
     imageUrl:
-      'https://cdn-mart.baemin.com/goods/custom/20200427/9751-main-01.png',
+      'https://product-image.kurly.com/product/image/4849ce2d-9cbd-4566-91c6-a85a4e71a523.jpg',
   },
   {
-    id: 39,
-    name: '식자재왕 김말이튀김 야채맛 1kg',
-    price: 6580,
+    id: 1000481383,
+    name: '[휘태커스] 미니슬랩 팝핑 트위츠  초콜릿 180g',
+    price: 10900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/bulk/20211210-155432/9416-main-01.jpg',
+      'https://product-image.kurly.com/product/image/c260f377-e462-41c6-b3de-9330949b48c5.jpg',
   },
   {
-    id: 40,
-    name: '삼양 야채튀김 (60g*50±1/박스) 3kg',
-    price: 24610,
+    id: 1000481385,
+    name: '[휘태커스] 미니슬랩 호키포키크런치 초콜릿 180g',
+    price: 10900,
     imageUrl:
-      'https://cdn-mart.baemin.com/sellergoods/main/8916edff-9fa9-4538-95c3-13d463f58a86.jpg',
+      'https://product-image.kurly.com/product/image/76891b83-9cd9-4d54-8be0-a897960524ce.jpg',
+  },
+  {
+    id: 1000485778,
+    name: '[매일식탁] 진한 사골육수가 들어간 소곱창전골',
+    price: 23900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/a639a1d6-1aed-4b74-b85b-fb6a0036cfe2.jpg',
+  },
+  {
+    id: 1000475133,
+    name: '군밤용 칼집 약단밤 500g',
+    price: 9900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/4730bf11-a28b-4963-b863-d713f2729590.jpg',
+  },
+  {
+    id: 1000471057,
+    name: '[삼립] 촉촉한 케익 10입(치즈/고구마 택1)',
+    price: 10900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/d69bcf53-de7e-4f4a-9d51-59eabcdb47fc.jpg',
+  },
+  {
+    id: 1000156832,
+    name: '[KF365] 친환경 블루베리 200g',
+    price: 19800,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/0316f3de-0cba-4cf8-92f4-88c0c27cf126.jpg',
+  },
+  {
+    id: 1000466107,
+    name: '[농심]사천짜파게티 4입',
+    price: 4580,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/6fd3987c-e6d4-4f70-b243-dfc255b5bb3a.jpg',
+  },
+  {
+    id: 5003437,
+    name: '델라웨어 포도 200g',
+    price: 15900,
+    imageUrl: 'https://img-cf.kurly.com/shop/data/goods/1619661214577l0.jpg',
+  },
+  {
+    id: 1000464795,
+    name: '[가로팔로] 이탈리아  IGP 파스타면 8종',
+    price: 3980,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/8616d03f-b552-4227-b666-931ef60e2fef.jpg',
+  },
+  {
+    id: 1000481375,
+    name: '[휘태커스] 미니슬랩 페퍼민트 다크초콜릿 180g',
+    price: 10900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/32e8fe85-d491-405c-a0a1-082b91bca47c.jpg',
+  },
+  {
+    id: 1000476796,
+    name: '[매일] 매일두유 고단백 (190mL X 12팩)',
+    price: 9980,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/a35b07ab-1eb1-4abc-8ee7-4117a55dbd77.jpg',
+  },
+  {
+    id: 1000452520,
+    name: '[빼바] 리얼 초콜릿 프로틴바 2종 (택2) ',
+    price: 2700,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/584b7d1e-e436-4b01-b851-b01be7eafb90.jpg',
+  },
+  {
+    id: 1000481369,
+    name: '[휘태커스] 미니슬랩 피넛 밀크초콜릿 180g',
+    price: 10900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/17232aba-b788-468f-9284-d2b56176d132.jpg',
+  },
+  {
+    id: 1000465939,
+    name: '[오뚜기]쇠고기미역국라면_컵 (60GX6)',
+    price: 6980,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/005aee52-f119-45db-a14f-418bf05c713e.jpg',
+  },
+  {
+    id: 1000464011,
+    name: '[기꼬만] 숙성 혼미림 1L',
+    price: 10980,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/c09a66e6-4f43-42fa-942e-4575bd18944a.jpg',
+  },
+  {
+    id: 1000428252,
+    name: '[달롤] 글루텐프리 가나슈 롤케이크 2종 (택1)',
+    price: 11500,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/a950e44e-d390-4ff6-a8f8-095d23840e10.jpg',
+  },
+  {
+    id: 1000484517,
+    name: '[마니커F&G] 버팔로윙 골드',
+    price: 9980,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/cd2e07a6-9ebf-4fc2-b17b-15553c704417.jpg',
+  },
+  {
+    id: 1000481381,
+    name: '[휘태커스] 미니슬랩 아몬드크랜베리 초콜릿 180g',
+    price: 10900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/87c11c30-808a-4495-b93f-00d8ff6d2fd6.jpg',
+  },
+  {
+    id: 1000482393,
+    name: '[위니비니] 반반 젤리박스 395g',
+    price: 12400,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/691c028d-1567-48e0-b6e6-b69ae6ec6ed4.jpg',
+  },
+  {
+    id: 1000487997,
+    name: '[애슐리] 크런치즈엣지 칠리쉬림프 피자',
+    price: 11900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/49e814dc-f5f4-40f8-a85c-4bd22be1332e.jpg',
+  },
+  {
+    id: 1000476799,
+    name: '[파사카] 벨지안 초코 크림치즈 바 2종',
+    price: 15900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/faa259d2-de5d-46f2-9a39-8a1763d5027f.jpg',
+  },
+  {
+    id: 1000482231,
+    name: '[쉐이크베이비] 다이어트 단백질 쉐이크 750g 10종 (택1)',
+    price: 42800,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/ed6bedbf-b598-4111-8459-5ef8846fde6d.jpg',
+  },
+  {
+    id: 1000456920,
+    name: '[조 말론 런던] 카 디퓨저 하우스 & 리필 4종(택1)',
+    price: 60000,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/6dc1b15f-e415-4a18-9869-c84b034cac41.jpg',
+  },
+  {
+    id: 1000487833,
+    name: '[에코맘의산골이유식] 산골푸딩 3종 (택1)',
+    price: 3400,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/c6719991-fb3d-4b23-84fd-8ed71fc45226.jpg',
+  },
+  {
+    id: 1000428262,
+    name: '[달롤] 글루텐프리 녹차 롤케이크 2종 (택1)',
+    price: 11500,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/e1a6dc2b-6c0c-4534-bc3e-7ba8c24a87f1.jpg',
+  },
+  {
+    id: 1000484515,
+    name: '[청정원] 순살바삭 허니간장치킨 300g',
+    price: 7980,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/9ef118bd-6acd-4e45-b0d9-5b2d3df30e85.jpg',
+  },
+  {
+    id: 1000481373,
+    name: '[휘태커스] 미니슬랩 크리미밀크 초콜릿 180g',
+    price: 10900,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/a31d2b4e-1782-4bbe-bbe4-33047b4609a8.jpg',
+  },
+  {
+    id: 1000480226,
+    name: '[매일] 허쉬 초콜릿 드링크 235mL X 4개입)',
+    price: 4680,
+    imageUrl:
+      'https://product-image.kurly.com/product/image/2c7685b0-6eef-40f5-94a7-66729da157da.jpg',
   },
 ];
