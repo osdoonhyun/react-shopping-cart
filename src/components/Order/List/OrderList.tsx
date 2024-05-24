95b9f542cadb4ebe834312babd3d7e8c27912ef7
osdoonhyun <skw1996@naver.com>
2024-05-24 16:14:15 +0900

    feat: 주문 내역 없는 경우 빈 상태 메시지 표시 기능 추가

--- src/components/Order/List/OrderList.tsx
+++ src/components/Order/List/OrderList.tsx
@@ -7,15 +7,24 @@ interface OrderListProps {
 }
 
 export default function OrderList({ orders }: OrderListProps) {
+  const isExistingOrders = !orders || orders.length > 0;
   return (
     <>
-      {orders?.map(({ id, orderDetails }: Order) => (
-        <div key={id} className='order-list'>
-          <OrderHeader id={id} detailButton />
+      {isExistingOrders ? (
+        <>
+          {orders?.map(({ id, orderDetails }: Order) => (
+            <div key={id} className='order-list'>
+              <OrderHeader id={id} detailButton />
 
-          <OrderDetailItem orderList={orderDetails} />
+              <OrderDetailItem orderList={orderDetails} />
+            </div>
+          ))}
+        </>
+      ) : (
+        <div className='order-empty'>
+          <p className='order-empty-message'>주문 내역이 없습니다.</p>
         </div>
-      ))}
+      )}
     </>
   );
 }
